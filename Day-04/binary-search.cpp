02675cd22a3da25510def0f6bcde3120d258a58c
Rahul <chaudharyraul07@gmail.com>
2026-08-15 15:23:47 +0530

    Day 04: Implemented Binary Search in C++

---
+++ Day-04/binary-search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int target = 70;
+
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            cout << "Element found at index: " << mid << endl;
+            break;
+        }
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return 0;
+}
