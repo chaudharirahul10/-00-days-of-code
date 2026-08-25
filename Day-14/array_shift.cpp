81f39e9f2b45909b4b677532f0b6bc46e8900834
Rahul <304365229+chaudharirahul10@users.noreply.github.com>
2026-08-25 18:38:05 +0530

    move array

---
+++ Day-14/array_shift.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int first = arr[0];
+    for (int i=0;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=first;
+    cout<<"Day 14 - Array after shifting: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
