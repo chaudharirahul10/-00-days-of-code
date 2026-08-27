cbe962b502c91b6e9d6b8ace170b85283da60a24
Rahul <304365229+chaudharirahul10@users.noreply.github.com>
2026-08-27 19:35:39 +0530

    Day 16: left rotate array

--- Day-16/left_rotate.cpp
+++ Day-16/left_rotate.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 
 int main(){
-    int arr[]={1,2,3,4,5};
-    int n=sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int first=arr[0];
-    for(int i=0;i<n-1;i++){
-        arr[i]=arr[i+1];
+    int first = arr[0];
+
+    for(int i = 0; i < n - 1; i++){
+        arr[i] = arr[i + 1];
     }
-    arr[n-1]=first;
-    cout<<"Array after left rotation: ";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+
+    arr[n - 1] = first;
+
+    cout << "Array after left rotation is : ";
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
     }
+
     return 0;
 }
